c6221a0a6f6caa8b9e31076633e255b740021ff8
liuluheng <872287087@qq.com>
2017-10-31 20:16:07 +0800

    log

--- npl_mod/Raft/RaftServer.lua
+++ npl_mod/Raft/RaftServer.lua
@@ -541,7 +541,7 @@ function RaftServer:handleAppendEntriesResponse(response)
     -- and the role was updated by UpdateTerm call
     -- Try to match up the logs for this peer
     if (self.role == ServerRole.Leader and needToCatchup) then
-        self.logger.debug("server %d needToCatchup %d < %d", peer:getId(), peer.nextLogIndex, self.logStore:getFirstAvailableIndex())
+        self.logger.debug("Server %d needToCatchup %d < %d", peer:getId(), peer.nextLogIndex, self.logStore:getFirstAvailableIndex())
         self:requestAppendEntries(peer);
     end
 end
