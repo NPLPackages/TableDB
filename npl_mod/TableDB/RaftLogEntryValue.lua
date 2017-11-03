ed0cc9f3380d847d5a25f644ceb00761ca81de69
liuluheng <872287087@qq.com>
2017-11-03 19:50:35 +0800

    reuse memoryFile and remove tmp table

--- npl_mod/TableDB/RaftLogEntryValue.lua
+++ npl_mod/TableDB/RaftLogEntryValue.lua
@@ -1,28 +1,30 @@
 --[[
-Title: 
+Title:
 Author: liuluheng
 Date: 2017.04.12
-Desc: 
+Desc:
 
 
 ------------------------------------------------------------
 NPL.load("(gl)npl_mod/TableDB/RaftLogEntryValue.lua");
 local RaftLogEntryValue = commonlib.gettable("TableDB.RaftLogEntryValue");
 ------------------------------------------------------------
-]]--
-
+]]
+--
 NPL.load("(gl)script/ide/commonlib.lua");
 local RaftLogEntryValue = commonlib.gettable("TableDB.RaftLogEntryValue");
 
-function RaftLogEntryValue:new(query_type, collection, query, index, serverId, enableSyncMode, callbackThread) 
+local memoryFile = ParaIO.open("<memory>", "w");
+
+function RaftLogEntryValue:new(query_type, collection, query, index, serverId, enableSyncMode, callbackThread)
     local o = {
-      query_type = query_type, 
-      collection = collection,
-      query = query,
-      cb_index = index,
-      serverId = serverId,
-      enableSyncMode = enableSyncMode,
-      callbackThread = callbackThread,
+        query_type = query_type,
+        collection = collection:ToData(),
+        query = query,
+        cb_index = index,
+        serverId = serverId,
+        enableSyncMode = enableSyncMode,
+        callbackThread = callbackThread,
     };
     setmetatable(o, self);
     return o;
@@ -38,21 +40,21 @@ end
 
 
 function RaftLogEntryValue:fromBytes(bytes)
-    local file = ParaIO.open("<memory>", "w");
-    if(file:IsValid()) then  
+    if (memoryFile:IsValid()) then
+        memoryFile:seek(0)
         if type(bytes) == "string" then
-            file:write(bytes, #bytes);
+            memoryFile:write(bytes, #bytes);
         elseif type(bytes) == "table" then
-            file:WriteBytes(#bytes, bytes);
+            memoryFile:WriteBytes(#bytes, bytes);
         end
-        file:seek(0)
-
-        local n = file:ReadInt();
-        local str = file:ReadString(n)
+        memoryFile:seek(0)
+        
+        local n = memoryFile:ReadInt();
+        local str = memoryFile:ReadString(n)
         -- print(str)
         local o = commonlib.LoadTableFromString(str)
         if not o then
-            str = string.gsub( str, "([%+%-][%a+%+%-]+)", "[\"%1\"]")
+            str = string.gsub(str, "([%+%-][%a+%+%-]+)", "[\"%1\"]")
             o = commonlib.LoadTableFromString(str)
         end
         setmetatable(o, self);
@@ -62,42 +64,25 @@ end
 
 
 function RaftLogEntryValue:toBytes()
-   local data = {
-       query_type = self.query_type,
-       collection = self.collection:ToData(),
-       query = self.query,
-       cb_index = self.cb_index,
-       serverId = self.serverId,
-       enableSyncMode = self.enableSyncMode,
-       callbackThread = self.callbackThread,
-   }
-   local str = commonlib.serialize_compact2(data)
-   -- print(str)
-   -- "<memory>" is a special name for memory file, both read/write is possible.
-   local file = ParaIO.open("<memory>", "w");
-   local bytes;
-   if(file:IsValid()) then
+    local str = commonlib.serialize_compact2(self)
+    
+    local bytes;
+    if (memoryFile:IsValid()) then
         -- -- query_type
-        -- file:WriteInt(#self.query_type)
-        -- file:WriteString(self.query_type)
-
+        -- memoryFile:WriteInt(#self.query_type)
+        -- memoryFile:WriteString(self.query_type)
         -- -- collection
-        -- file:WriteInt(#self.collection.name)
-        -- file:WriteString(self.collection.name)
-        -- file:WriteInt(#self.collection.db)
-        -- file:WriteString(self.collection.db)
-
+        -- memoryFile:WriteInt(#self.collection.name)
+        -- memoryFile:WriteString(self.collection.name)
+        -- memoryFile:WriteInt(#self.collection.db)
+        -- memoryFile:WriteString(self.collection.db)
         -- query
         -- query is a dict or a nested dict
-
-        file:WriteInt(#str)
-        file:WriteString(str)
-
-
-        bytes = file:GetText(0, -1)
-
-        file:close()
+        memoryFile:seek(0)
+        memoryFile:WriteInt(#str)
+        memoryFile:WriteString(str)
+        
+        bytes = memoryFile:GetText(0, -1)
     end
     return bytes;
 end
-
