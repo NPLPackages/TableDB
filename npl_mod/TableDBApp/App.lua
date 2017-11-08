a074946777a1fbe65e8749b010947623cf73f15b
liuluheng <872287087@qq.com>
2017-11-08 10:24:21 +0800

    disable snapshot

--- npl_mod/TableDBApp/App.lua
+++ npl_mod/TableDBApp/App.lua
@@ -97,7 +97,7 @@ local function executeInServerMode(stateMachine)
     raftParameters.maximumAppendingSize = 200;
     raftParameters.logSyncBatchSize = 5;
     raftParameters.logSyncStopGap = 5;
-    raftParameters.snapshotDistance = 50000;
+    raftParameters.snapshotDistance = -1;
     raftParameters.snapshotBlockSize = 0;
 
     local rpcListener = RpcListener:new(listenIp, listenPort, thisServer.id, config.servers, raftThreadName)
