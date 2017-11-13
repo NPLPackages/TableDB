9e3a7a6e4407f9a77572db635deeaf074c4f1d7a
liuluheng <872287087@qq.com>
2017-11-13 14:36:09 +0800

    format using vscode-lua

--- npl_mod/Raft/Logger.lua
+++ npl_mod/Raft/Logger.lua
@@ -1,36 +1,36 @@
 --[[
-Title: 
+Title:
 Author: liuluheng
 Date: 2017.03.25
-Desc: 
+Desc:
 
 
 ------------------------------------------------------------
 NPL.load("(gl)npl_mod/Raft/Logger.lua");
 local Logger = commonlib.gettable("Raft.Logger");
 ------------------------------------------------------------
-]]--
+]] --
 
-local Logger = commonlib.gettable("Raft.Logger");
+local Logger = commonlib.gettable("Raft.Logger")
 
 function Logger:new(modname)
-    local module_name = modname or ""
-    local logger = commonlib.logging.GetLogger(module_name);
-    logger.level = "INFO"
-    local function appender(level, ...)
-        logger.std(nil, level, module_name, ...)
-    end
+  local module_name = modname or ""
+  local logger = commonlib.logging.GetLogger(module_name)
+  logger.level = "INFO"
+  local function appender(level, ...)
+    logger.std(nil, level, module_name, ...)
+  end
 
-    logger.setAppender(appender)
-    local o = logger;
-    setmetatable(o, self);
-    return o;
+  logger.setAppender(appender)
+  local o = logger
+  setmetatable(o, self)
+  return o
 end
 
 function Logger:__index(name)
-    return rawget(self, name) or Logger[name];
+  return rawget(self, name) or Logger[name]
 end
 
 function Logger:__tostring()
-    return util.table_tostring(self)
+  return util.table_tostring(self)
 end
