552c39d2ad18610bab9cad28a537a4c072183dbd
liuluheng <872287087@qq.com>
2018-02-27 16:59:05 +0800

    vscode-format

--- npl_mod/Raft/ServerState.lua
+++ npl_mod/Raft/ServerState.lua
@@ -9,38 +9,36 @@ Desc:
 NPL.load("(gl)npl_mod/Raft/ServerState.lua");
 local ServerState = commonlib.gettable("Raft.ServerState");
 ------------------------------------------------------------
-]]--
+]] --
 
-NPL.load("(gl)script/ide/commonlib.lua");
+NPL.load("(gl)script/ide/commonlib.lua")
 
-local ServerState = commonlib.gettable("Raft.ServerState");
+local ServerState = commonlib.gettable("Raft.ServerState")
 
-function ServerState:new(t, c, v) 
-    local o = {
-        term = t or 0,
-        commitIndex = c or -1,
-        votedFor = v or 0,
-    };
-    setmetatable(o, self);
-    return o;
+function ServerState:new(t, c, v)
+  local o = {
+    term = t or 0,
+    commitIndex = c or -1,
+    votedFor = v or 0
+  }
+  setmetatable(o, self)
+  return o
 end
 
 function ServerState:__index(name)
-    return rawget(self, name) or ServerState[name];
+  return rawget(self, name) or ServerState[name]
 end
 
 function ServerState:__tostring()
-    return util.table_tostring(self)
+  return util.table_tostring(self)
 end
-
 
 function ServerState:increaseTerm()
-    self.term = self.term + 1
+  self.term = self.term + 1
 end
 
-
 function ServerState:setCommitIndex(commitIndex)
-    if (commitIndex > self.commitIndex) then
-        self.commitIndex = commitIndex;
-    end
+  if (commitIndex > self.commitIndex) then
+    self.commitIndex = commitIndex
+  end
 end
