18c08d8d3da9ca205447a021d118774e476f01b6
donglishuo <donglishuo@163.com>
2018-04-23 21:58:46 +0800

    make image local

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -7,7 +7,8 @@ if [ "$1" == "client" ]; then
 	cp ./init-cluster.json ./client/cluster.json
 	cp ./tabledb.config.xml ./client/temp/test_raft_database/tabledb.config.xml
 	echo server.id=11 > ./client/config.properties
-	docker run -v $PWD/client:/app/tabledb/setup/server/ --network mynet --ip 172.16.0.4 -d xuntian/npl-tabledb client 
+	# docker run -v $PWD/client:/app/tabledb/setup/server/ --network mynet --ip 172.16.0.4 -d xuntian/npl-tabledb client 
+	docker run -v $PWD/client:/app/tabledb/setup/server/ --network mynet --ip 172.16.0.4 -d tabledb client 
 else 
 	for i in 11 12 13
 	do
@@ -16,7 +17,8 @@ else
 	mkdir ./server$i
 	cp ./init-cluster.json ./server$i/cluster.json
 	echo server.id=$i > ./server$i/config.properties
-	docker run -v $PWD/server$i:/app/tabledb/setup/server/ --network mynet --ip 172.16.0.$i -d xuntian/npl-tabledb server $i
+	# docker run -v $PWD/server$i:/app/tabledb/setup/server/ --network mynet --ip 172.16.0.$i -d xuntian/npl-tabledb server $i
+	docker run -v $PWD/server$i:/app/tabledb/setup/server/ --network mynet --ip 172.16.0.$i -d tabledb server $i
 	echo server$i has been ready
 	done
 fi
